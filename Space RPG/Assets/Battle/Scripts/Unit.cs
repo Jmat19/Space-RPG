e0241f622d0060e9793ed2aaaa590ae2ea6b6383
Jmat19 <47995875+Jmat19@users.noreply.github.com>
2023-03-23 17:15:13 -0400

    Second-Save-BS

---
+++ Space RPG/Assets/Battle/Scripts/Unit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHealth;
+    public int currentHealth;
+
+    public bool TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+
+        if (currentHealth <= 0)
+            return true;
+        else
+            return false;
+    }
+}
